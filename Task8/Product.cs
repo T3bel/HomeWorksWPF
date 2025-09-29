562b0bd286007e925467cb6a6be689cc1a4ed929
T3bel <drepslol@gmail.com>
2025-09-29 23:47:58 +0300

    Выполнено домашнее задание 8

---
+++ Task8/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    public enum Category
+    {
+        Food,
+        Appliances
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public Category Category { get; set; }
+    }
+}
