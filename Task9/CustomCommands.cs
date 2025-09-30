a97003d524458d20eed8a2a0419f36b28e247b69
T3bel <drepslol@gmail.com>
2025-09-30 10:51:34 +0300

    Выполнено задание блока 9. Бонусом сделал тулбар с предыдущим цветом, чтобы не держать его в голове

---
+++ Task9/CustomCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task9
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand ChangeColor = new RoutedUICommand(
+            "Изменить цвет",
+            "ChangeColor",
+            typeof(CustomCommands),
+            new InputGestureCollection
+            {
+            new KeyGesture(Key.C, ModifierKeys.Control)
+            });
+    }
+}
